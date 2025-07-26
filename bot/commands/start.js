7441c45c1e2a804a882e78973faeab0da802d486
Mohizanur <rco47840@gmail.com>
2025-07-26 21:33:06 +0300

    Fix circular reference error in debugging

--- bot/commands/start.js
+++ bot/commands/start.js
@@ -6,6 +6,11 @@ const userHandlers = require("../handlers/userHandlers");
 
 module.exports = async (ctx) => {
   console.log("🚀 /start command received from user:", ctx.from.id);
-  console.log("📝 Context:", JSON.stringify(ctx, null, 2));
+  console.log("📝 User info:", {
+    id: ctx.from.id,
+    username: ctx.from.username,
+    firstName: ctx.from.first_name,
+    lastName: ctx.from.last_name
+  });
   return userHandlers.handleStart(ctx);
 };
